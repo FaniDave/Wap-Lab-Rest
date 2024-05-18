af01d9579a16ffb46732baa31936c889ad471236
FaniDave <130611031+FaniDave@users.noreply.github.com>
2024-05-18 01:57:08 -0500

    Update app.js
    Error handled

--- app.js
+++ app.js
@@ -7,5 +7,10 @@ const app = express();
 app.use(express.json());
 app.use("/books", BookRouter);
 
+//Error handling 
+app.use((err, req, res, next) => {
+     res.status(500).json({success: false, message: err.message});
+});
+
 //listen 3000 
 app.listen(3000, () => console.log("Listening on 3000"));
